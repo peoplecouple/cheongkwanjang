736bf4b3cab3847598cc7cf76aab6e1036e7a9da
peoplecouple <peoplecouple@naver.com>
2022-08-12 08:59:07 +0900

    20220812

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function () {
+  $('#fullpage').fullpage({
+
+  })
+
+  $('.Story .slider').slick({
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    pauseOnHover: false,
+    pauseOnFocus: false,
+  })
+})
