c8d61c18c2834717607ff8ee41a0da1b6690109b
peoplecouple <peoplecouple@naver.com>
2022-08-12 17:40:00 +0900

    20220812

--- js/main.js
+++ js/main.js
@@ -1,14 +1,28 @@
 $(function () {
   $('#fullpage').fullpage({
+    controlArrows: false,
+    slidesNavigation: true,
+    slidesNavPosition: 'bottom',
+    loopHorizontal: false,
 
-  })
+    anchors: ['p01', 'p02', 'p03', 'p04', 'p05', 'P06'],
 
-  $('.Story .slider').slick({
-    arrows: false,
-    dots: true,
-    autoplay: true,
-    autoplaySpeed: 4000,
-    pauseOnHover: false,
-    pauseOnFocus: false,
-  })
+    afterLoad: function (origin, destination) {
+
+      destination.anchor == 'p03' ? $('.Won').addClass('on') : $('.Won').removeClass('on')
+      
+    }
+
+  });
+
+  //슬라이드 마우스휠로 움직이게
+  $(".Story").on("wheel", function (e) {
+    if (e.originalEvent.wheelDelta / 120 > 0) {
+      fullpage_api.moveSlideLeft();
+    }
+    else {
+      fullpage_api.moveSlideRight();
+    }
+  });
+
 })
